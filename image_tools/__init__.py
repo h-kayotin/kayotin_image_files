ee2e98fc0d7258146b9d67fc10019a7c8f438294
JiangHai <hanayo@kayotin.cn>
2023-05-11 16:03:27 +0800

    0511

--- image_tools/__init__.py
+++ image_tools/__init__.py
@@ -4,3 +4,4 @@ __init__.py -
 Author: JiangHai江海
 Date： 2023/5/11
 """
+from image_tools import png_ico
--- kayotin_main.py
+++ kayotin_main.py
@@ -5,3 +5,6 @@ Author: JiangHai江海
 Date： 2023/5/11
 """
 
+import image_tools
+
+image_tools.png_ico.ico_main()

00103f0bbd530154e18a0242149938878d8c8bc7
JiangHai <hanayo@kayotin.cn>
2023-06-16 17:53:54 +0800

    0616

--- image_tools/find_new.py
+++ image_tools/find_new.py
@@ -26,20 +26,21 @@ class FileSearchEngine(ttk.Frame):
 
         # 这里是绝对路径，/分隔，默认是当前路径
         _path = pathlib.Path().absolute().as_posix()
-        self.path_var = ttk.StringVar(value=_path)
-        self.term_var = ttk.StringVar(value='')
-        self.type_var = ttk.StringVar(value='contains')
+        self.path_var = ttk.StringVar(value=_path)  # 默认路径填写在文件夹路径中
+        self.term_var = ttk.StringVar(value='')  # 默认关键字，为空
+        self.type_var = ttk.StringVar(value='contains')  # 默认搜索模式，contains
 
-        # header and labelframe option container
+        # 创建顶部的Labelframe块
         option_text = "请选择或输入文件夹路径，输入关键词开始搜索"
         self.option_lf = ttk.Labelframe(self, text=option_text, padding=15)
         self.option_lf.pack(fill=X, expand=YES, anchor=N)
 
-        self.create_path_row()
-        self.create_term_row()
-        self.create_type_row()
-        self.create_results_view()
+        self.create_path_row()  # 创建路径那一行
+        self.create_term_row()  # 创建选择关键字那一行
+        self.create_type_row()  # 创建选择搜索类型那一行
+        self.create_results_view()  # 创建搜索结果显示的块
 
+        # 创建进度条
         self.progressbar = ttk.Progressbar(
             master=self, 
             mode=INDETERMINATE, 
@@ -51,14 +52,14 @@ class FileSearchEngine(ttk.Frame):
         """在labelframe中加入文件路径选择那一行"""
         path_row = ttk.Frame(self.option_lf)
         path_row.pack(fill=X, expand=YES)
-        path_lbl = ttk.Label(path_row, text="文件夹路径", width=10)
+        path_lbl = ttk.Label(path_row, text="文件夹路径", width=10)  # label 文件夹路径
         path_lbl.pack(side=LEFT, padx=(15, 0))
-        path_ent = ttk.Entry(path_row, textvariable=self.path_var)
+        path_ent = ttk.Entry(path_row, textvariable=self.path_var)  # 输入框
         path_ent.pack(side=LEFT, fill=X, expand=YES, padx=5)
-        browse_btn = ttk.Button(
+        browse_btn = ttk.Button(      # 浏览按钮
             master=path_row, 
             text="浏览",
-            command=self.on_browse, 
+            command=self.on_browse,   # 绑定方法，获取路径
             width=8
         )
         browse_btn.pack(side=LEFT, padx=5)
@@ -118,12 +119,13 @@ class FileSearchEngine(ttk.Frame):
         self.result_view = ttk.Treeview(
             master=self,
             style="info",
-            columns=["0", 1, 2, 3, 4],
+            columns=["#0", "#1", "#2", "#3", "#4"],  # 列表长度代表列数
             show=HEADINGS
         )
         self.result_view.pack(fill=BOTH, expand=YES, pady=10)
 
         # setup columns and use `scale_size` to adjust for resolution
+        # 初始化列标题，用scale_size去调整列宽
         self.result_view.heading(0, text='文件名', anchor=W)
         self.result_view.heading(1, text='最后修改时间', anchor=W)
         self.result_view.heading(2, text='文件类型', anchor=E)
@@ -133,7 +135,7 @@ class FileSearchEngine(ttk.Frame):
             column=0, 
             anchor=W, 
             width=utility.scale_size(self, 125), 
-            stretch=False
+            stretch=True
         )
         self.result_view.column(
             column=1, 
@@ -172,24 +174,25 @@ class FileSearchEngine(ttk.Frame):
         for item in items:
             self.result_view.delete(item)
 
-        search_term = self.term_var.get()
-        search_path = self.path_var.get()
-        search_type = self.type_var.get()
+        search_term = self.term_var.get()  # 关键字
+        search_path = self.path_var.get()  # 路径
+        search_type = self.type_var.get()  # 搜索类型
 
-        if search_term == '':
+        if search_term == '':  # 如果关键字为空
             return
 
         # start search in another thread to prevent UI from locking
-        # 新建一个进程来进行搜索
+        # 新建一个线程来进行搜索
         Thread(
             target=FileSearchEngine.file_search, 
             args=(search_term, search_path, search_type), 
             daemon=True
         ).start()
-        self.progressbar.start(10)
+        self.progressbar.start(10)  # 进度条滚动
 
         iid = self.result_view.insert(
-            parent='', 
+            # 插入在根进节点，插入在最后
+            parent='',
             index=END, 
         )
         self.result_view.item(iid, open=True)
@@ -201,7 +204,7 @@ class FileSearchEngine(ttk.Frame):
             child.destroy()
         kayotin_main.main(self.master)
 
-    def check_queue(self, iid):
+    def check_queue(self, iid):  # 这个方法还要研究下
         """检查队列，然后进行显示"""
         if all([
             FileSearchEngine.searching, 
@@ -254,9 +257,9 @@ class FileSearchEngine(ttk.Frame):
     @staticmethod  # 静态方法表示不和类相关的方法
     def file_search(term, search_path, search_type):
         """根据搜索类型分别调用方法进行搜索"""
-        FileSearchEngine.set_searching(True)
+        FileSearchEngine.set_searching(True)  # 这个参数表示搜索在进行中
         if search_type == 'contains':
-            FileSearchEngine.find_contains(term, search_path)
+            FileSearchEngine.find_contains(term, search_path)  # 执行搜索的主方法
         elif search_type == 'endswith':
             FileSearchEngine.find_endswith(term, search_path)
 
@@ -266,8 +269,8 @@ class FileSearchEngine(ttk.Frame):
         src_path = pathlib.Path(search_path)
         result = list(src_path.rglob(f"*{term}*"))
         for file in result:
-            FileSearchEngine.queue.put(file)
-        FileSearchEngine.set_searching(False)
+            FileSearchEngine.queue.put(file)  # 将搜索结果放到队列中
+        FileSearchEngine.set_searching(False)  # 将状态设置为结束
 
     @staticmethod
     def find_endswith(term, search_path):
